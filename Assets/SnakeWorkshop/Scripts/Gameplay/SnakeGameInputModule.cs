f0b3a802d8f472c69698e71c10c292399faba632
Ömür Efe Güçlü <omurefeguclu2004@gmail.com>
2024-12-11 04:09:04 +0300

    Update SnakeGameInputModule.cs

--- Assets/SnakeWorkshop/Scripts/Gameplay/SnakeGameInputModule.cs
+++ Assets/SnakeWorkshop/Scripts/Gameplay/SnakeGameInputModule.cs
@@ -17,19 +17,19 @@ namespace SnakeWorkshop.Scripts.Gameplay
         public void OnUpdate()
         {
             // Catch the input from the player and update the game data accordingly
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             {
                 _gameData.SetMovementDirection(MovementDirection.Up);
             }
-            else if (Input.GetKeyDown(KeyCode.S))
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
             {
                 _gameData.SetMovementDirection(MovementDirection.Down);
             }
-            else if (Input.GetKeyDown(KeyCode.A))
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
                 _gameData.SetMovementDirection(MovementDirection.Left);
             }
-            else if (Input.GetKeyDown(KeyCode.D))
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
             {
                 _gameData.SetMovementDirection(MovementDirection.Right);
             }
